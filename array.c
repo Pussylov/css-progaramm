ac5fc71fb915e3f1dd87df4b2d918e15b13f7c26
Pussylov <2101030600031@silveroakuni.ac.in>
2022-08-11 14:43:26 +0530

    first css

---
+++ array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a[10],i,n,x=0;
+    printf("enter array size:\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("enter no :");
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a)
+    }
+    
+    return 0;
+}
